1ab9d7877d8a4ddd37e3f1f7b33f90a8dbee23ba
Rinda <haythem_t@outlook.fr>
2025-08-14 22:52:52 +0100

    Day 4 add sections, inactive record, color, auto number

---
+++ UsrRinda/Schemas/UsrPokemon_FormPage/UsrPokemon_FormPage.js
@@ -0,0 +1,397 @@
+define("UsrPokemon_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "CardToggleTabPanel",
+				"values": {
+					"styleType": "default",
+					"bodyBackgroundColor": "primary-contrast-500",
+					"selectedTabTitleColor": "auto",
+					"tabTitleColor": "auto",
+					"underlineSelectedTabColor": "auto",
+					"headerBackgroundColor": "auto"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "Feed",
+				"values": {
+					"dataSourceName": "PDS",
+					"entitySchemaName": "UsrPokemon"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "AttachmentList",
+				"values": {
+					"columns": [
+						{
+							"id": "2755daae-88e8-4d31-b304-83e1a439b734",
+							"code": "AttachmentListDS_Name",
+							"caption": "#ResourceString(AttachmentListDS_Name)#",
+							"dataValueType": 28,
+							"width": 200
+						}
+					]
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "UsrName",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 1,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.UsrName",
+					"control": "$UsrName",
+					"labelPosition": "auto",
+					"multiline": false
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Price",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 2,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.PDS_UsrPrice_g3lfqsy",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrPrice_g3lfqsy"
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "PokemonRegnumber",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 3,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.PDS_UsrRegnumber_zwp6o22",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrRegnumber_zwp6o22",
+					"multiline": false,
+					"visible": true,
+					"readonly": true,
+					"placeholder": "",
+					"tooltip": ""
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "BattleType",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.PDS_UsrBattleType_890r98z",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrBattleType_890r98z",
+					"listActions": [],
+					"showValueAsLink": false,
+					"controlActions": [],
+					"visible": true,
+					"readonly": false,
+					"placeholder": "",
+					"tooltip": "",
+					"valueDetails": null
+				},
+				"parentName": "GeneralInfoTabContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "PokemonStatus",
+				"values": {
+					"layoutConfig": {
+						"column": 2,
+						"colSpan": 1,
+						"row": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.PDS_UsrStatus_p3frigw",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrStatus_p3frigw",
+					"listActions": [],
+					"showValueAsLink": false,
+					"controlActions": [],
+					"visible": true,
+					"readonly": false,
+					"placeholder": "",
+					"tooltip": "",
+					"valueDetails": null
+				},
+				"parentName": "GeneralInfoTabContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "PokemonHeight",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 2,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.PDS_UsrHeight_p3nfbbv",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrHeight_p3nfbbv"
+				},
+				"parentName": "GeneralInfoTabContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "PokemonTrainer",
+				"values": {
+					"layoutConfig": {
+						"column": 2,
+						"colSpan": 1,
+						"row": 2,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.PDS_UsrTrainer_jtohn8l",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrTrainer_jtohn8l",
+					"listActions": [],
+					"showValueAsLink": false,
+					"controlActions": [],
+					"visible": true,
+					"readonly": false,
+					"placeholder": "",
+					"tooltip": "",
+					"valueDetails": null
+				},
+				"parentName": "GeneralInfoTabContainer",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "PokemonWeight",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 3,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.PDS_UsrWeight_9i1amr3",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrWeight_9i1amr3"
+				},
+				"parentName": "GeneralInfoTabContainer",
+				"propertyName": "items",
+				"index": 4
+			},
+			{
+				"operation": "insert",
+				"name": "PokemonOwner",
+				"values": {
+					"layoutConfig": {
+						"column": 2,
+						"colSpan": 1,
+						"row": 3,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.PDS_UsrOwner_ab7bcti",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrOwner_ab7bcti",
+					"listActions": [],
+					"showValueAsLink": false,
+					"controlActions": [],
+					"visible": true,
+					"readonly": false,
+					"placeholder": "",
+					"tooltip": "",
+					"valueDetails": null
+				},
+				"parentName": "GeneralInfoTabContainer",
+				"propertyName": "items",
+				"index": 5
+			},
+			{
+				"operation": "insert",
+				"name": "PokemonBattleCount",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 4,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.PDS_UsrBattleCount_bafj4g8",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrBattleCount_bafj4g8"
+				},
+				"parentName": "GeneralInfoTabContainer",
+				"propertyName": "items",
+				"index": 6
+			},
+			{
+				"operation": "insert",
+				"name": "PokemonPokedexDescription",
+				"values": {
+					"layoutConfig": {
+						"column": 2,
+						"colSpan": 1,
+						"row": 4,
+						"rowSpan": 1
+					},
+					"type": "crt.RichTextEditor",
+					"label": "$Resources.Strings.PDS_UsrPokedexDescription_by9hzyb",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrPokedexDescription_by9hzyb",
+					"multiline": false,
+					"filesStorage": {
+						"masterRecordColumnValue": "$Id",
+						"entitySchemaName": "SysFile",
+						"recordColumnName": "RecordId"
+					}
+				},
+				"parentName": "GeneralInfoTabContainer",
+				"propertyName": "items",
+				"index": 7
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"UsrName": {
+						"modelConfig": {
+							"path": "PDS.UsrName"
+						}
+					},
+					"PDS_UsrPrice_g3lfqsy": {
+						"modelConfig": {
+							"path": "PDS.UsrPrice"
+						}
+					},
+					"PDS_UsrBattleType_890r98z": {
+						"modelConfig": {
+							"path": "PDS.UsrBattleType"
+						}
+					},
+					"PDS_UsrStatus_p3frigw": {
+						"modelConfig": {
+							"path": "PDS.UsrStatus"
+						}
+					},
+					"PDS_UsrHeight_p3nfbbv": {
+						"modelConfig": {
+							"path": "PDS.UsrHeight"
+						}
+					},
+					"PDS_UsrWeight_9i1amr3": {
+						"modelConfig": {
+							"path": "PDS.UsrWeight"
+						}
+					},
+					"PDS_UsrBattleCount_bafj4g8": {
+						"modelConfig": {
+							"path": "PDS.UsrBattleCount"
+						}
+					},
+					"PDS_UsrTrainer_jtohn8l": {
+						"modelConfig": {
+							"path": "PDS.UsrTrainer"
+						}
+					},
+					"PDS_UsrOwner_ab7bcti": {
+						"modelConfig": {
+							"path": "PDS.UsrOwner"
+						}
+					},
+					"PDS_UsrPokedexDescription_by9hzyb": {
+						"modelConfig": {
+							"path": "PDS.UsrPokedexDescription"
+						}
+					},
+					"PDS_UsrRegnumber_zwp6o22": {
+						"modelConfig": {
+							"path": "PDS.UsrRegnumber"
+						}
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"path": [
+					"attributes",
+					"Id",
+					"modelConfig"
+				],
+				"values": {
+					"path": "PDS.Id"
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [],
+				"values": {
+					"primaryDataSourceName": "PDS"
+				}
+			},
+			{
+				"operation": "merge",
+				"path": [
+					"dataSources"
+				],
+				"values": {
+					"PDS": {
+						"type": "crt.EntityDataSource",
+						"config": {
+							"entitySchemaName": "UsrPokemon"
+						},
+						"scope": "page"
+					}
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
---
+++ UsrRinda/Schemas/UsrPokemon_ListPage/UsrPokemon_ListPage.js
@@ -0,0 +1,186 @@
+define("UsrPokemon_ListPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "MenuItem_ImportFromExcel",
+				"values": {
+					"clicked": {
+						"request": "crt.ImportDataRequest",
+						"params": {
+							"entitySchemaName": "UsrPokemon"
+						}
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "FolderTree",
+				"values": {
+					"rootSchemaName": "UsrPokemon"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "DataTable",
+				"values": {
+					"columns": [
+						{
+							"id": "f252f581-0ccf-44ac-b7c9-c00df2ad9919",
+							"code": "PDS_UsrName",
+							"caption": "#ResourceString(PDS_UsrName)#",
+							"dataValueType": 1,
+							"sticky": true,
+							"width": 108
+						},
+						{
+							"id": "5137206f-6612-dd08-857a-1e15d9602904",
+							"code": "PDS_UsrTrainer",
+							"caption": "#ResourceString(PDS_UsrTrainer)#",
+							"dataValueType": 10,
+							"width": 144
+						},
+						{
+							"id": "2c7b1c45-e4bf-6aa5-12d1-d3b1ff33cf16",
+							"code": "PDS_UsrPrice",
+							"caption": "#ResourceString(PDS_UsrPrice)#",
+							"dataValueType": 32,
+							"width": 131
+						},
+						{
+							"id": "6dfec74b-0368-f1c2-20de-7f8b637d7bfa",
+							"code": "PDS_UsrBattleType",
+							"caption": "#ResourceString(PDS_UsrBattleType)#",
+							"dataValueType": 10,
+							"width": 133
+						},
+						{
+							"id": "134a1527-edcb-ddb9-d3c4-5f2c52f8eb8c",
+							"code": "PDS_UsrStatus",
+							"caption": "#ResourceString(PDS_UsrStatus)#",
+							"dataValueType": 10,
+							"width": 102
+						},
+						{
+							"id": "1891ab11-10a7-576b-3211-aba283b2503b",
+							"code": "PDS_UsrHeight",
+							"caption": "#ResourceString(PDS_UsrHeight)#",
+							"dataValueType": 31,
+							"width": 128
+						},
+						{
+							"id": "36039507-51fa-a74a-daf5-649457381354",
+							"code": "PDS_UsrWeight",
+							"caption": "#ResourceString(PDS_UsrWeight)#",
+							"dataValueType": 31,
+							"width": 135
+						}
+					]
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "Dashboards",
+				"values": {
+					"_designOptions": {
+						"entitySchemaName": "UsrPokemon",
+						"dependencies": [
+							{
+								"attributePath": "Id",
+								"relationPath": "PDS.Id"
+							}
+						],
+						"filters": []
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes",
+					"Items",
+					"viewModelConfig",
+					"attributes"
+				],
+				"values": {
+					"PDS_UsrName": {
+						"modelConfig": {
+							"path": "PDS.UsrName"
+						}
+					},
+					"PDS_UsrTrainer": {
+						"modelConfig": {
+							"path": "PDS.UsrTrainer"
+						}
+					},
+					"PDS_UsrPrice": {
+						"modelConfig": {
+							"path": "PDS.UsrPrice"
+						}
+					},
+					"PDS_UsrBattleType": {
+						"modelConfig": {
+							"path": "PDS.UsrBattleType"
+						}
+					},
+					"PDS_UsrStatus": {
+						"modelConfig": {
+							"path": "PDS.UsrStatus"
+						}
+					},
+					"PDS_UsrHeight": {
+						"modelConfig": {
+							"path": "PDS.UsrHeight"
+						}
+					},
+					"PDS_UsrWeight": {
+						"modelConfig": {
+							"path": "PDS.UsrWeight"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"dataSources",
+					"PDS",
+					"config"
+				],
+				"values": {
+					"entitySchemaName": "UsrPokemon",
+					"attributes": {
+						"UsrName": {
+							"path": "UsrName"
+						},
+						"UsrTrainer": {
+							"path": "UsrTrainer"
+						},
+						"UsrPrice": {
+							"path": "UsrPrice"
+						},
+						"UsrBattleType": {
+							"path": "UsrBattleType"
+						},
+						"UsrStatus": {
+							"path": "UsrStatus"
+						},
+						"UsrHeight": {
+							"path": "UsrHeight"
+						},
+						"UsrWeight": {
+							"path": "UsrWeight"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
